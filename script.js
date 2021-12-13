2fc0ac7a57d125acc6a8c6a5dec650f53854dfcc
rileyadrienne <96031527+rileyadrienne@users.noreply.github.com>
2021-12-12 20:24:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principle = document.getElementById("principal").value;
+    var rate= document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principle * years * rate/100;
+    var year= new Date().getFullyear()+parseInt(years);
 }
+function updaterate()
+{
+    var rateval = document.getElemendById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;}
         
