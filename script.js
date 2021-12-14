4d84682b92ced4cc00447246d887a71934e386e2
rileyadrienne <96031527+rileyadrienne@users.noreply.github.com>
2021-12-13 17:22:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principle * years * rate/100;
     var year= new Date().getFullyear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit";
+    var result= document.getElementById("result").value;
+   document.getElementById("result").innerHTML="If you deposit";
 }
 function updateRate() 
 {
