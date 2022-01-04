10a9d73f7e6a349f7e77f1024a570b00df647eab
rileyadrienne <96031527+rileyadrienne@users.noreply.github.com>
2022-01-03 18:00:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ function updateRate()
  document.getElementById("rate_val").innerText=rateval;
 }
 
+
 function compute()
-{
-     var principle = document.getElementById("principal").value;
-    var rate= document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var year= new Date().getFullyear()+parseInt(years);
-     var interest = principle * years * rate/100;
-document.getElementById("result").innerHTML="If you deposit "+principal+"<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>";
+{ var principal = document.getElementById("principal").value;
+ if (principal<=0){
+     alert("enter a positive number");
+     principal.focus;
+     return false;
+ }
+var rate = document.getElementById("rate").value
+var years = document.getElementById("years").value;
+var interest = principal * years * rate/100;
+var year = new Date().getFullYear()+parseInt(years);
+
+document.getElementById("result").innerHTML="If you deposit "+principal+"<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>"
 }
