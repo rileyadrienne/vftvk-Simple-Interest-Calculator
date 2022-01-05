3f2ccd7ac234140072ec347e6e767f73dc3430be
rileyadrienne <96031527+rileyadrienne@users.noreply.github.com>
2022-01-04 21:12:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,3 @@ var year = new Date().getFullYear()+parseInt(years);
 
 document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark><br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 }
-
